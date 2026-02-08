02911e216fefd414a3c7a2e3a53012f9947567cf
Andrew Forrest <andrew@dysphoria.net>
2026-02-08 12:39:35 +0000

    Add INT8_TO_STRING: stringifies unit8
    * Single-digit numbers (0–9) are very efficient (we have to special-
      case zero anyway).
    * Does ‘division’ by multiple subtraction, but for the 10s, test for
      >50, to reduce number of subtractions required. (TBH, this
      optimisation will have more of an effect when we have to deal with
      16 and 32-bit numbers, when subtraction is more expensive.)

---
+++ rom/functions.test.ts
@@ -0,0 +1,49 @@
+// Test the startup sequence:
+import { describe, it } from "jsr:@std/testing/bdd";
+import {asString, loadVm, Vm} from './testutils/testvm.ts'
+import { expect } from "jsr:@std/expect/expect";
+import type { byte } from "../zxsys/Byte.ts";
+import { rom } from "./generated/symbols.ts";
+
+const loadedVm = loadVm()
+
+describe("INT8_TO_STRING", () => {
+	function rendersInt(intValue: byte) {
+		it(`Renders ${intValue}`, async () => {
+			const vm = await loadedVm
+
+			const result = int8ToString(vm, intValue)
+
+			expect(result).toBe(intValue.toString())
+		})
+	}
+	rendersInt(0)
+	rendersInt(9)
+	rendersInt(10)
+	rendersInt(11)
+	rendersInt(15)
+	rendersInt(16)
+	rendersInt(19)
+	rendersInt(20)
+	rendersInt(42)
+	rendersInt(49)
+	rendersInt(50)
+	rendersInt(51)
+	rendersInt(99)
+	rendersInt(100)
+	rendersInt(101)
+	rendersInt(110)
+	rendersInt(160)
+	rendersInt(199)
+	rendersInt(201)
+	rendersInt(249)	// Takes the most time.
+	rendersInt(255)
+})
+
+function int8ToString(vm: Vm, intValue: byte): string {
+	const buffer = 0x5800
+	vm.setRegisters({ A: intValue, HL: buffer })
+	vm.callSubroutine(rom.INT8_TO_STRING, 431)
+	const {HL} = vm.getRegisters()
+	return asString(vm.getRam(buffer, HL - buffer))
+}
