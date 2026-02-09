8e79c6de91c07f1790cdcbce5c8f70cfd4ce50a1
Andrew Forrest <andrew@dysphoria.net>
2026-02-09 14:53:43 +0000

    16-bit int to string

--- rom/functions.test.ts
+++ rom/functions.test.ts
@@ -40,10 +40,41 @@ describe("INT8_TO_STRING", () => {
 	rendersInt(255)
 })
 
+describe("INT16_TO_STRING", () => {
+	function rendersInt(intValue: number) {
+		it(`Renders ${intValue}`, async () => {
+			const vm = await loadedVm
+
+			const result = int16ToString(vm, intValue)
+
+			expect(result).toBe(intValue.toString())
+		})
+	}
+	rendersInt(0)
+	rendersInt(9)
+	rendersInt(10)
+	rendersInt(255)
+	rendersInt(501)
+	rendersInt(1000)
+	rendersInt(9999)
+	rendersInt(10101)
+	rendersInt(59999)
+	rendersInt(65534)
+	rendersInt(65535)
+})
+
 function int8ToString(vm: Vm, intValue: byte): string {
 	const buffer = 0x5800
 	vm.setRegisters({ A: intValue, HL: buffer })
-	vm.callSubroutine(rom.INT8_TO_STRING, 424)
+	vm.callSubroutine(rom.INT8_TO_STRING, 444)
+	const {HL} = vm.getRegisters()
+	return asString(vm.getRam(buffer, HL - buffer))
+}
+
+function int16ToString(vm: Vm, intValue: number): string {
+	const buffer = 0x5800
+	vm.setRegisters({ BC: intValue, HL: buffer })
+	vm.callSubroutine(rom.INT16_TO_STRING, 1250)
 	const {HL} = vm.getRegisters()
 	return asString(vm.getRam(buffer, HL - buffer))
 }
